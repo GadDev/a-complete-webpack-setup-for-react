9c558eb61c0ad66763a42f4b7e9620d8bef89cab
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-19 15:07:00 +0100

    fix test

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# a-complete-webpack-setup-for-react
+# Cap React starter kit
+
+## Install dependencies
+
+`yarn install`
+
+## Run the project
+
+`yarn start`
+
+## Build project
+
+`yarn build`
+
+## Run tests
+
+`yarn test`
+
+## Lint project
+
+`yarn lint`
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+
 import MyImage from "../assets/image.jpeg";
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my-webpack-react-starter</h1>
+      <h1>Welcome to the Cap React starter kit</h1>
       <img src={MyImage} />
     </div>
   );
